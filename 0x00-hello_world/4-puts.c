f51e83274b294611adaa23fdd4940c9c2e1ddf3a
Okeke Christian <eceokekechristian@gmail.com>
2022-12-12 03:49:09 -0800

    Welcome to C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
- * main - print a particular line of code using C Progamming
- * Return = 0
+/**
+ * main -print a line of code using puts.
+ *
+ * Return: 0
  */
-int main(void)
+ int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
-
